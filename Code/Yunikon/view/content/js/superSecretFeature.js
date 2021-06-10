921f7905ad84b215733f65774dbe977e75451721
JAQUIER Cyprien <Cyprien.JAQUIER@cpnv.ch>
2021-06-10 16:57:55 +0200

    starting to make event page dynamic
    starting to make event page dynamic

--- Code/Yunikon/view/content/js/superSecretFeature.js
+++ Code/Yunikon/view/content/js/superSecretFeature.js
@@ -38,7 +38,6 @@ document.addEventListener('keydown', function(e) {
   });
 
 function easterStart() {
-    document.getElementById("logoSuperSecretFeature").src = "https://cdn.streamlabs.com/users/41445587/library/1552671673728.gif";
-    document.getElementById("header").style.background = "url(/view/content/images/dancin.mp4)";
+    document.getElementById("header").style.background = "url()";
     document.getElementById("videoDiv").removeAttribute("hidden");
 }
