cc7bc726f1d96846493d12dc676208332d012ad2
Cyprien-png <61788240+Cyprien-png@users.noreply.github.com>
2021-06-17 18:17:08 +0200

    comment controller page
    comment controller page

--- Code/Yunikon/controller/controller.php
+++ Code/Yunikon/controller/controller.php
@@ -23,11 +23,13 @@ function register()
 
 function lost()
 {
+    //go to the error 404 page
     require "view/Lost.php";
 }
 
 function login()
-{
+{   
+    //go to the login page
     $errorLogin = "";
     require "view/login.php";
 }
@@ -35,17 +37,19 @@ function login()
 
 function createEvent($eventData)
 {
-    //(isset($eventData['addName']) && isset($eventData['addStarting']) && isset($eventData['addEnding']) && isset($eventData['addLocation'])) {
-    //extract login parameters
+
+    //set event datas
     $eventName = $eventData['addName'];
     $eventStarting = $eventData['addStarting'];
     $eventEnding = $eventData['addEnding'];
     $eventLocation = $eventData['addLocation'];
     $eventDescription = $eventData['addDescription'];
 
+    //set tickets datas
     $ticketNumber = $eventData['ticketsNumber'];
     $ticketPrice = $eventData['ticketsPrice'];
 
+    //check if the image is available
     $file_name = $_FILES['addImage']['name'];
     $file_tmp = $_FILES['addImage']['tmp_name'];
     $extension = pathinfo($_FILES["addImage"]["name"], PATHINFO_EXTENSION);
@@ -57,15 +61,18 @@ function createEvent($eventData)
         header("Location: /home");
     }
 
+    //add event to the db
     require_once "model/model.php";
     registerEvent($eventName, $eventStarting, $eventEnding, $eventLocation, $eventDescription, $name);
+
+    //get event id
     $eventInfos = getEventId($eventName);
     $eventId = $eventInfos[0]['id'];
-
+    //add the true number of ticket in db
     for ($i = 0; $i < $ticketNumber; $i++) {
         registerTicket($ticketPrice, $eventId);
     }
-
+    //go to the home page
     home();
 }
 
@@ -73,7 +80,6 @@ function createEvent($eventData)
 
 function registerRequest($registerData)
 {
-    echo $registerData['lastname'], ",", $registerData['firstname'], ",", $registerData['email'], ",", $registerData['password'], ",", $registerData['passwordConfirm'], ",", $registerData['phone'];
 
     $errorRegister = "";
     if (!empty($registerData['email']) && !empty($registerData['password']) && !empty($registerData['passwordConfirm'])) {
@@ -137,6 +143,7 @@ function loginRequest($loginData)
 
 function createSession($userEmailAddress, $firstname, $lastname, $exhibitor, $phone)
 {
+    //set session var
     $_SESSION['userEmailAddress'] = $userEmailAddress;
     $_SESSION['firstname'] = $firstname;
     $_SESSION['lastname'] = $lastname;
@@ -146,7 +153,7 @@ function createSession($userEmailAddress, $firstname, $lastname, $exhibitor, $ph
 
 function sessionId()
 {
-
+    //get current user's id
     $userEmailAddress = $_SESSION['userEmailAddress'];
     require_once "model/model.php";
     $userInfo = getUserInfo($userEmailAddress);
@@ -156,14 +163,17 @@ function sessionId()
 
 function eventList()
 {
+    //get next event date and every events
     require_once "model/model.php";
     $nextEvent = getNextEvent();
     $items = showEvent();
+    //go to the event list page
     require "view/eventList.php";
 }
 
 function logout()
 {
+    //clear the sessions vars
     $_SESSION = array();
     session_destroy();
     header("Location: /home");
@@ -171,6 +181,7 @@ function logout()
 
 function exhibitor()
 {
+    //check if current user is an exhibitor
     require_once "./model/model.php";
     $res = getUserExhibitor(@$_SESSION['userEmailAddress']);
     return $res;
@@ -178,19 +189,21 @@ function exhibitor()
 
 function account()
 {
+    //get current user's datas
     require_once "model/model.php";
     $user = getUserInfoByPhone($_SESSION['phoneNumber']);
     $userId = $user[0]['id'];
 
+    //go to the account page
     $items = getEventByUserId($userId);
     require "view/account.php";
 }
 
 function deleteEvent($eventId){
-
+    //delete event by id 
     require_once "model/model.php";
     delEvent($eventId);
-
+    //return to account page
     account();
 }
 
@@ -205,30 +218,34 @@ function event($eventId)
     foreach ($tickets as $count){
         $remaining ++;
     }
-    
+    //set current date
     $date = date("Y-m-d");
 
+    //get ticket price
     $price = $tickets[0]['price'];
     $eventData = getEventById($eventId);
     if ($eventData == false) {
         home();
     } else {
+        //go to the event page
         require "view/event.php";
     }
 }
 
 function addEvent()
 {
+    //go to the creating event page
     require "view/add-event.php";
 }
 function contact()
 {
+    //go to the contact page
     require "view/contact.php";
 }
 
 function sendMail($infoMail)
 {
-
+    //create and send an email
     if ($infoMail['exposant'] == "on") {
         $header = "Demande d'exposant";
         $message = $_SESSION['userEmailAddress'] . " - " . $infoMail['nom'] . " souhaite convertir son compte pour devenir exposant sur Yunikon !";
@@ -244,7 +261,7 @@ function sendMail($infoMail)
     }
 
     require_once "PHPMailer/PHPMailerAutoload.php";
-
+    //set mailer datas
     $mail = new PHPMailer();
 
     $mail->isSMTP();
@@ -272,6 +289,7 @@ function sendMail($infoMail)
 
 function getToken($userInfo)
 {
+    //create and check user's token 
     $errorForgot = "";
     $errorToken = "";
     if (!empty($userInfo['email'])) {
@@ -307,6 +325,7 @@ function getToken($userInfo)
 
 function forgotPassword()
 {
+    //go to the forgot password page
     $errorToken = "";
     $errorForgot = "";
     require "view/forgot_password.php";
@@ -398,6 +417,7 @@ function forgotPasswordRequest($userInfo)
 
 function changeRegister()
 {
+    //go to the change account's datas page
     $errorMsg = "";
     $successMsg = "";
     require "view/Change-infos.php";
